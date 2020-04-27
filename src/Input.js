17ac5fe8d44a8a0ed334e5125980230a4c5231ac
anu radha <anu.sivasubramanian@gmail.com>
2020-04-27 14:46:38 +0200

    added checkPRops for the input.test.js

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Input() {
+/**
+ * Functional react component for input field.
+ * @function
+ * @param {object} props - react props
+ * @returns {JSX.element} -
+ */
+
+const Input = (props) => {
   return <div data-test="component-input"></div>;
-}
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import { shallow } from "enzyme";
 import Input from "./Input";
 
@@ -7,12 +7,19 @@ import Input from "./Input";
  * setup function for input component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const defaultProps = { secretWord: "party" };
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 test("renders without error", () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, "component-input");
   expect(inputComponent.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { secretWord: "party" };
+  checkProps(Input, expectedProps);
 });
